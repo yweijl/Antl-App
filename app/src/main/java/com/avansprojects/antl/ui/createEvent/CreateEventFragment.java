6840b6ced78dd812a241805a59838a8fa6c1c5be
Yoshua <y.weijl@rdc.nl>
2019-01-06 16:38:25 +0100

    antl-166: adding a savebutton to create event

--- app/src/main/java/com/avansprojects/antl/ui/createEvent/CreateEventFragment.java
+++ app/src/main/java/com/avansprojects/antl/ui/createEvent/CreateEventFragment.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.avansprojects.antl.R;
 
@@ -24,6 +25,20 @@ public class CreateEventFragment extends Fragment {
     private static final int NUM_PAGES = 4;
     private ViewPager mPager;
     private PagerAdapter mPagerAdapter;
+    private TextView _nameTextView;
+    private TextView _DescriptionTextView;
+    private TextView _firstDateTextView;
+    private TextView _secondDateTextView;
+    private TextView _thirdDateTextView;
+    private TextView _fourthDateTextView;
+    private TextView _firstTimeTextView;
+    private TextView _secondTimeTextView;
+    private TextView _thirdTimeTextView;
+    private TextView _fourthTimeTextView;
+    private Button _nextButton;
+    private Button _saveButton;
+    private ImageButton _backButton;
+    private int _pictureLocation;
 
     public static CreateEventFragment newInstance() {
         return new CreateEventFragment();
@@ -38,20 +53,46 @@ public class CreateEventFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState){
+        findEventViews();
+        setEventButtons();
+        setEventAdapter();
+    }
 
-        Button _nextButton = getActivity().findViewById(R.id.createEventNextButton);
-        _nextButton.setOnClickListener(v -> {
-            mPager.setCurrentItem(getItem(+1), true);
-        });
-
-        ImageButton _backButton = getActivity().findViewById(R.id.createEventBackButton);
-        _backButton.setOnClickListener(v -> {
-            mPager.setCurrentItem(getItem(-1), true);
-        });
-
+    private void setEventAdapter() {
         mPager = getView().findViewById(R.id.CreateEventPager);
         mPagerAdapter = new ScreenSlidePagerAdapter(getFragmentManager());
         mPager.setAdapter(mPagerAdapter);
+    }
+
+    private void setEventButtons() {
+
+        _saveButton = getActivity().findViewById(R.id.createEventNextButton);
+        _saveButton.setOnClickListener(v -> { });
+
+        _nextButton = getActivity().findViewById(R.id.createEventNextButton);
+        _nextButton.setOnClickListener(v -> {
+            int newPosition = getItem(+1);
+            setButtonVisibility(newPosition);
+            mPager.setCurrentItem(newPosition, true);
+        });
+
+        _backButton = getActivity().findViewById(R.id.createEventBackButton);
+        _backButton.setOnClickListener(v -> {
+            mPager.setCurrentItem(getItem(-1), true);
+        });
+    }
+
+    private void findEventViews() {
+        _nameTextView = getView().findViewById(R.id.enterEventName);
+        _DescriptionTextView = getView().findViewById(R.id.enterEventDescription);
+        _firstDateTextView = getView().findViewById(R.id.firstEventDate);
+        _secondDateTextView = getView().findViewById(R.id.secondEventDate);
+        _thirdDateTextView = getView().findViewById(R.id.thirdEventDate);
+        _fourthDateTextView = getView().findViewById(R.id.fourthEventDate);
+        _firstTimeTextView = getView().findViewById(R.id.firstEventTime);
+        _secondTimeTextView = getView().findViewById(R.id.secondEventTime);
+        _thirdTimeTextView = getView().findViewById(R.id.thirdEventTime);
+        _fourthTimeTextView = getView().findViewById(R.id.fourthEventTime);
     }
 
     @Override
@@ -62,8 +103,22 @@ public class CreateEventFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
-    private int getItem(int i) {
-        return mPager.getCurrentItem() + i;
+    private void setButtonVisibility(int position) {
+        if (position == 3) {
+            _nextButton.setVisibility(View.GONE);
+            _saveButton.setVisibility(View.VISIBLE);
+        } else {
+            if (_nextButton.getVisibility() == View.GONE) {
+                _nextButton.setVisibility(View.VISIBLE);
+            }
+            if (_saveButton.getVisibility() == View.VISIBLE) {
+                _nextButton.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    private int getItem(int position) {
+        return mPager.getCurrentItem() + position;
     }
 
     private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
