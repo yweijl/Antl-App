9dad673254c55cab9ae7a143b70c7eca56e5ba08
Yoshua <y.weijl@rdc.nl>
2019-01-06 12:38:07 +0100

    antl-166: Added option to add camera image

--- app/src/main/java/com/avansprojects/antl/ui/createEvent/CreateEventImageFragment.java
+++ app/src/main/java/com/avansprojects/antl/ui/createEvent/CreateEventImageFragment.java
@@ -1,15 +1,37 @@
 package com.avansprojects.antl.ui.createEvent;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import com.avansprojects.antl.R;
+import com.avansprojects.antl.helpers.GlideApp;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import static android.app.Activity.RESULT_OK;
+
 public class CreateEventImageFragment extends Fragment {
+
+    String mCurrentPhotoPath;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    private ImageView mImageView;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -20,5 +42,77 @@ public class CreateEventImageFragment extends Fragment {
 
         return rootView;
     }
-    
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mImageView = Objects.requireNonNull(getView()).findViewById(R.id.addPictureImage);
+        Button button = Objects.requireNonNull(getActivity()).findViewById(R.id.addImage);
+        button.setOnClickListener(x -> {
+            try {
+                createImageFile();
+                dispatchTakePictureIntent();
+                galleryAddPic();
+                GlideApp.with(this).load(mCurrentPhotoPath).centerCrop().into(mImageView);
+            } catch (IOException ex){
+                System.out.printf("Error %s", ex);
+            }
+        });
+    }
+//
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+//            Bundle extras = data.getExtras();
+//            GlideApp.with(this).load(extras.get("data")).centerCrop().into(mImageView);
+//        }
+//    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Objects.requireNonNull(getActivity()).getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(Objects.requireNonNull(getActivity()).getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                System.out.printf("An error occured while invoking the camera intent %s", ex);
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(Objects.requireNonNull(getContext()),
+                        "com.avansprojects.antl.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        Objects.requireNonNull(getActivity()).sendBroadcast(mediaScanIntent);
+    }
 }
+
