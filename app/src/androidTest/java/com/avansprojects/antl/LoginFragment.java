8c86d1e8f004df25f2ef77499a25a4b98ac44232
Bas Onstwedder <sl.onstwedder@avans.nl>
2019-01-20 22:20:12 +0100

    antl-198: espresso test fixed

--- app/src/androidTest/java/com/avansprojects/antl/LoginFragment.java
+++ app/src/androidTest/java/com/avansprojects/antl/LoginFragment.java
@@ -67,54 +67,27 @@ public class LoginFragment {
     @Test
     public void successfulLoginNavigatesToEventsOverview() {
         ViewInteraction textView = onView(
-                allOf(withId(R.id.LoginTextView), withText("LOGIN"),
-                        childAtPosition(
-                                allOf(withId(R.id.constraintLayout),
-                                        childAtPosition(
-                                                IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
-                                                0)),
-                                1),
+                allOf(withId(R.id.LoginTextView),
                         isDisplayed()));
         textView.check(matches(isDisplayed()));
 
         ViewInteraction textInputEditText = onView(
                 allOf(withId(R.id.username_text_input),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.textInputLayout),
-                                        0),
-                                0),
                         isDisplayed()));
         textInputEditText.perform(click());
 
         ViewInteraction textInputEditText2 = onView(
                 allOf(withId(R.id.username_text_input),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.textInputLayout),
-                                        0),
-                                0),
                         isDisplayed()));
         textInputEditText2.perform(replaceText("tester"), closeSoftKeyboard());
 
         ViewInteraction textInputEditText3 = onView(
                 allOf(withId(R.id.password_edit_text),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.password_text_input),
-                                        0),
-                                0),
                         isDisplayed()));
         textInputEditText3.perform(replaceText("Test@123"), closeSoftKeyboard());
 
         ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.next_button), withText("Login"),
-                        childAtPosition(
-                                allOf(withId(R.id.constraintLayout),
-                                        childAtPosition(
-                                                withClassName(is("android.widget.FrameLayout")),
-                                                0)),
-                                4),
+                allOf(withId(R.id.next_button),
                         isDisplayed()));
         appCompatButton.perform(click());
 
@@ -154,23 +127,11 @@ public class LoginFragment {
 
         ViewInteraction appCompatButton2 = onView(
                 Matchers.allOf(withId(R.id.logoutButton), withText("Logout"),
-                        childAtPosition(
-                                Matchers.allOf(withId(R.id.constraintLayout),
-                                        childAtPosition(
-                                                withId(R.id.nav_host_fragment),
-                                                0)),
-                                1),
                         isDisplayed()));
         appCompatButton2.perform(click());
 
         textView = onView(
                 Matchers.allOf(withId(R.id.LoginTextView), withText("LOGIN"),
-                        childAtPosition(
-                                Matchers.allOf(withId(R.id.constraintLayout),
-                                        childAtPosition(
-                                                IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
-                                                0)),
-                                1),
                         isDisplayed()));
         textView.check(matches(isDisplayed()));
     }
@@ -179,80 +140,31 @@ public class LoginFragment {
     public void unsuccessfulLoginShowsToast() {
         ViewInteraction textView = onView(
                 allOf(withId(R.id.LoginTextView), withText("LOGIN"),
-                        childAtPosition(
-                                allOf(withId(R.id.constraintLayout),
-                                        childAtPosition(
-                                                IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
-                                                0)),
-                                1),
                         isDisplayed()));
         textView.check(matches(isDisplayed()));
 
         ViewInteraction textInputEditText = onView(
                 allOf(withId(R.id.username_text_input),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.textInputLayout),
-                                        0),
-                                0),
                         isDisplayed()));
         textInputEditText.perform(click());
 
         ViewInteraction textInputEditText2 = onView(
                 allOf(withId(R.id.username_text_input),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.textInputLayout),
-                                        0),
-                                0),
                         isDisplayed()));
         textInputEditText2.perform(replaceText("testor"), closeSoftKeyboard());
 
         ViewInteraction textInputEditText3 = onView(
                 allOf(withId(R.id.password_edit_text),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.password_text_input),
-                                        0),
-                                0),
                         isDisplayed()));
         textInputEditText3.perform(replaceText("Test@123"), closeSoftKeyboard());
 
         ViewInteraction appCompatButton = onView(
                 allOf(withId(R.id.next_button), withText("Login"),
-                        childAtPosition(
-                                allOf(withId(R.id.constraintLayout),
-                                        childAtPosition(
-                                                withClassName(is("android.widget.FrameLayout")),
-                                                0)),
-                                4),
                         isDisplayed()));
         appCompatButton.perform(click());
 
-        ViewInteraction viewGroup = onView(
-                allOf(withId(R.id.eventFragment),
-                        childAtPosition(
-                                allOf(withId(R.id.nav_host_fragment),
-                                        childAtPosition(
-                                                withId(R.id.container),
-                                                1)),
-                                0),
-                        isDisplayed()));
-        viewGroup.check(doesNotExist());
-
-        ViewInteraction imageButton = onView(
-                allOf(withId(R.id.createEventFab),
-                        childAtPosition(
-                                allOf(withId(R.id.eventFragment),
-                                        childAtPosition(
-                                                withId(R.id.nav_host_fragment),
-                                                0)),
-                                1),
-                        isDisplayed()));
-        imageButton.check(doesNotExist());
-
         onView(
-                withText("Login failed")).
+                withText("Login failed with error code: 400")).
                 inRoot(withDecorView(not(is(mActivityTestRule.getActivity().getWindow().getDecorView())))).
                 check(matches(isDisplayed()));
     }
